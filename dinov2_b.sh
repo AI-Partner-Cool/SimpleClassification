4249c9b6b7f554d25cd8a37a42ac0412d5a51a00
AI-Partner-Cool <ai.partner.cool@outlook.com>
2025-05-26 20:21:35 +0800

    dinov2 shell

---
+++ dinov2_b.sh
@@ -0,0 +1,56 @@
+### CARS + DINOV2-B
+
+python train.py \
+--save-dir CARS/dinov2_base_patch14_378_BS32_epoch150_Lr1e5 \
+--gpu 9 \
+--model dinov2_base_patch14 \
+--max-lr 1e-5 \
+--min-lr 1e-7 \
+--warmup-epoch 1 \
+--nb-epoch 150 \
+--train-size 378 \
+--batch-size 32 \
+--optimizer adamw \
+--test-size 378 CARS
+
+### CUB + DINOV2-B
+python train.py \
+--save-dir CUB/dinov2_base_patch14_378_BS32_epoch150_Lr1e5 \
+--gpu 9 \
+--model dinov2_base_patch14 \
+--max-lr 1e-5 \
+--min-lr 1e-7 \
+--warmup-epoch 1 \
+--nb-epoch 150 \
+--train-size 378 \
+--batch-size 32 \
+--optimizer adamw \
+--test-size 378 CUB
+
+## CIFAR10 + DINOV2-B
+python train.py \
+--save-dir CIFAR10/dinov2_base_patch14_378_BS32_epoch150_Lr1e5 \
+--gpu 9 \
+--model dinov2_base_patch14 \
+--max-lr 1e-5 \
+--min-lr 1e-7 \
+--warmup-epoch 1 \
+--nb-epoch 150 \
+--train-size 378 \
+--batch-size 32 \
+--optimizer adamw \
+--test-size 378 CIFAR10
+
+## CIFAR100 + DINOV2-B
+python train.py \
+--save-dir CIFAR100/dinov2_base_patch14_378_BS32_epoch150_Lr1e5 \
+--gpu 9 \
+--model dinov2_base_patch14 \
+--max-lr 1e-5 \
+--min-lr 1e-7 \
+--warmup-epoch 1 \
+--nb-epoch 150 \
+--train-size 378 \
+--batch-size 32 \
+--optimizer adamw \
+--test-size 378 CIFAR100
